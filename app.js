4fa327dfbb25e053f93986cf568f3f675ab253d6
anilcodersigdel <anilsigdeel594@gmail.com>
2024-08-07 23:28:21 +0545

    creating objects and adding list item in array

--- app.js
+++ app.js
@@ -1,34 +1,71 @@
-const inputBox = document.getElementById('inputBox');
-const addBtn = document.getElementById('addBtn');
-const todoList = document.getElementById('todoList');
+const inputBox = document.getElementById("inputBox");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+
 const todos = [];
 
-const addTodo = ()=> {
-    console.log('button clicked sucessfully');
+const addTodo = () => {
+  console.log("button clicked sucessfully");
+  const inputText = inputBox.value.trim();
 
-    const inputText = inputBox.value.trim();
+  if (inputText <= 0) {
+    alert("you must write something");
+    return false;
+  }
+  const uuid = self.crypto.randomUUID();
+  console.log(uuid);
 
-    if (inputText <= 0){
-        alert('you must write something');
+  const obj = {
+    label: inputText,
+    checked: false,
+    key: uuid,
+  };
 
-        return false;
-    }
+  todos.push(obj);
+  console.log(todos);
 
-    const listing = document.createElement('li');
+  todoList.innerHTML = "";
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.className = 'todo-checkbox';
+  display();
+};
 
+const display = () => {
+  todos.forEach((val) => {
+    const listing = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "todo-checkbox";
 
-    const paragraph = document.createElement('p');
-    paragraph.innerHTML = inputText;
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = val.label;
+
+    const editBtn = document.createElement("button");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+    editBtn.classList.add("btn", "editBtn");
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteBtn.id = "deleteBtn";
+    deleteBtn.classList.add("btn", "deleteBtn");
 
     listing.appendChild(checkbox);
     listing.appendChild(paragraph);
+    listing.appendChild(editBtn);
+    listing.appendChild(deleteBtn);
 
     todoList.appendChild(listing);
-    inputBox.value = '';
+    inputBox.value = "";
+  });
+};
 
-}
-addBtn.addEventListener('click', addTodo);
+addBtn.addEventListener("click", addTodo);
+console.log(todos);
+
+deleteBtn.addEventListener("click", function(event) {
+  console.log("delete button clicked sucessfully");
+  if (event.target.id === "deleteBtn") {
+    todoList.removeChild(event.target.parentElement);
+  }
+});
+
+// localStorage.setItem;
